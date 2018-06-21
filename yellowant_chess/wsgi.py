3dca77e3bc782d52286cebf66b818ea857ed09fd
Gaurav <gaurav.uttarkar@gmail.com>
2018-06-21 20:21:47 +0530

    Error in heroku

--- yellowant_chess/wsgi.py
+++ yellowant_chess/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yellowant_chess.settings")
 
+DEV_ENV = os.environ.get("ENV")
+if DEV_ENV == "heroku":
+    os.system('echo "from django.contrib.auth.models import User; User.objects.create_superuser(\'admin\', \'admin@example.com\', \'pass\')" | python manage.py shell')
+
 application = get_wsgi_application()
