edd1e79d4348240d8683c27b3f4ff7f1b8d21b1a
Gaurav <gaurav.uttarkar@gmail.com>
2018-06-21 21:07:58 +0530

    StockFish

--- lib/yellowant_command_center/commands.py
+++ lib/yellowant_command_center/commands.py
@@ -161,7 +161,7 @@ def playComputer(args,user_integration):
     """
     object = UserIntegration.objects.get(yellowant_integration_id=user_integration.yellowant_integration_id)
     board = chess.Board(object.board_state)
-    engine = chess.uci.popen_engine("/Users/Gaurav/Desktop/Yellowant/chess/Stockfish/src/stockfish")
+    engine = chess.uci.popen_engine("Stockfish/src/stockfish")
     col = color(object.board_state[-12])
     engine.position(board)
     move = engine.go(movetime=2000)
