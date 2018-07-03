4071b097def71c0bcbcdaf97b5f0fcfeafdc2a4c
Gaurav <gaurav.uttarkar@gmail.com>
2018-07-03 19:40:28 +0530

    Last commit

--- yellowant_chess/wsgi.py
+++ yellowant_chess/wsgi.py
@@ -20,9 +20,9 @@ print(DEV_ENV)
 if DEV_ENV == "heroku":
     os.system('echo "from django.contrib.auth.models import User; User.objects.create_superuser(\'admin\', \'admin@example.com\', \'pass\')" | python manage.py shell')
     #subprocess.call(['chmod 777 yellowant_chess/engine.sh'])
-    #os.chmod('/app/stockfish/Linux/stockfish-9',0o777)
-    os.chmod('/app/yellowant_chess/engine.sh',0o777)
-    os.system("/app/yellowant_chess/engine.sh")
+    os.chmod('/app/stockfish/Linux/stockfish-9',0o777)
+    # os.chmod('/app/yellowant_chess/engine.sh',0o777)
+    # os.system("/app/yellowant_chess/engine.sh")
     #subprocess.call(['yellowant_chess/engine.sh'])
 
 
