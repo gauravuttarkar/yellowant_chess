28e20a09fe62db4d9143dedad4e2571b22a8a052
Gaurav <gaurav.uttarkar@gmail.com>
2018-07-03 18:33:34 +0530

    Linux binary Stockfish added

--- yellowant_chess/engine.sh
+++ yellowant_chess/engine.sh
@@ -2,4 +2,4 @@ sudo apt-get install git g++
 echo "Hello"
 cd /app/Stockfish-master/src
 make help
-make build ARCH=x86-64
+make profile-build ARCH=x86-64
--- yellowant_chess/wsgi.py
+++ yellowant_chess/wsgi.py
@@ -20,13 +20,11 @@ print(DEV_ENV)
 if DEV_ENV == "heroku":
     os.system('echo "from django.contrib.auth.models import User; User.objects.create_superuser(\'admin\', \'admin@example.com\', \'pass\')" | python manage.py shell')
     #subprocess.call(['chmod 777 yellowant_chess/engine.sh'])
-    os.chmod('/app/yellowant_chess/engine.sh',0o777)
-    os.system("/app/yellowant_chess/engine.sh")
+    # os.chmod('/app/yellowant_chess/engine.sh',0o777)
+    # os.system("/app/yellowant_chess/engine.sh")
     #subprocess.call(['yellowant_chess/engine.sh'])
 
-else:
-    os.chmod('yellowant_chess/engine.sh',0o777)
-    os.system("yellowant_chess/engine.sh")
+
     #subprocess.call(['yellowant_chess/engine.sh'])
 
 application = get_wsgi_application()
